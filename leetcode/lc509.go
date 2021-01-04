930b167e62241b4c8e369667c7354705b7337ebf
zhengdx <zdx07@qq.com>
2021-01-04 20:38:20 +0800

    commit lc509

---
+++ leetcode/lc509.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	a, b := 0, 1
+	for i := 2; i <= n; i++ {
+		c := a + b
+		a = b
+		b = c
+	}
+	return b
+}
