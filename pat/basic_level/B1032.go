9acaae0db1f55b1429a23cc2802b78f77dbd63de
zhengdx <zdx07@qq.com>
2021-01-09 23:07:37 +0800

    commit B1032

--- pat/basic_level/B1032.go
+++ pat/basic_level/B1032.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var n, maxNum int
-	fmt.Scanf("%d\n", &n)
+	sc := bufio.NewScanner(os.Stdin)
+
+	n, maxNum, inputStrs := 0, 0, make([]string, 2)
+	sc.Scan()
+	n, _ = strconv.Atoi(sc.Text())
 
 	total := make([]int, n+1)
 	for i := 0; i < n; i++ {
 		var num, grade int
-		fmt.Scanf("%d %d\n", &num, &grade)
+		sc.Scan()
+		inputStrs = strings.Split(sc.Text(), " ")
+		num, _ = strconv.Atoi(inputStrs[0])
+		grade, _ = strconv.Atoi(inputStrs[1])
 		total[num] += grade
 		if total[maxNum] < total[num] {
 			maxNum = num
