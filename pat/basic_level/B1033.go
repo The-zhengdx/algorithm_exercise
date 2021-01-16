a141c42cea62c0a2b35d2105435f1208c9d3202c
zhengdx <zdx07@qq.com>
2021-01-16 21:40:40 +0800

    commit B1033

---
+++ pat/basic_level/B1033.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	brokenKeys := sc.Text()
+	sc.Scan()
+	inputStr := sc.Text()
+	brokenKeysMap := make(map[byte]bool)
+	for i, l := 0, len(brokenKeys); i < l; i++ {
+		brokenKeysMap[brokenKeys[i]] = true
+	}
+	for i, l := 0, len(inputStr); i < l; i++ {
+		key := inputStr[i]
+		if key >= 'a' && key <= 'z' {
+			key -= 'a' - 'A'
+		}
+		if brokenKeysMap[key] || brokenKeysMap['+'] && inputStr[i] >= 'A' && inputStr[i] <= 'Z' {
+			continue
+		}
+		fmt.Printf("%c", inputStr[i])
+	}
+	fmt.Println()
+}
