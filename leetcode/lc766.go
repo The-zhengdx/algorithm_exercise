2c525642c62ec3c33b0e083edc2a454f3efc6085
zhengdx <zdx07@qq.com>
2021-02-23 00:22:36 +0800

    commit lc766

--- leetcode/lc766.go
+++ leetcode/lc766.go
@@ -2,16 +2,9 @@ package leetcode
 
 func isToeplitzMatrix(matrix [][]int) bool {
 	m, n := len(matrix), len(matrix[0])
-	for i, j := 0, 0; i < m-1; i++ {
-		for r, c := i+1, j+1; r < m && c < n; r, c = r+1, c+1 {
-			if matrix[r][c] != matrix[i][j] {
-				return false
-			}
-		}
-	}
-	for i, j := 0, 1; j < n-1; j++ {
-		for r, c := i+1, j+1; r < m && c < n; r, c = r+1, c+1 {
-			if matrix[r][c] != matrix[i][j] {
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][j] != matrix[i-1][j-1] {
 				return false
 			}
 		}
